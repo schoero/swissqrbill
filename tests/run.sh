bc718ddaee4413310ad0928edc9afcdcef7b1b8c
Roger Schönbächler <schoero@gmx.ch>
2020-03-05 08:36:10 +0100

    Create output dir if not existing

--- tests/run.sh
+++ tests/run.sh
@@ -1,41 +1,42 @@
 #!/bin/sh
+mkdir output -p
 echo "ts"
-tsc ts.ts
- node ts
+  tsc ts.ts
+  node ts
  rm ts.js
 echo "a4"
- node a4
+  node a4
 echo "a6-5"
- node a6-5
+  node a6-5
 echo "a6-5-housenmuber"
- node a6-5-housenumber
+  node a6-5-housenumber
 echo "av1av2"
- node av1av2
+  node av1av2
 echo "english"
- node english
+  node english
 echo "euro"
- node euro
+  node euro
 echo "french"
- node french
+  node french
 echo "italian"
- node italian
+  node italian
 echo "multipage"
- node multipage
+  node multipage
 echo "no-amount"
- node no-amount
+  node no-amount
 echo "no-debitor"
- node no-debitor
+  node no-debitor
 echo "no-debitor-no-amount"
- node no-debitor-no-amount
+  node no-debitor-no-amount
 echo "no-debitor-no-amount-no-reference"
- node no-debitor-no-amount-no-reference
+  node no-debitor-no-amount-no-reference
 echo "no-debitor-no-reference"
- node no-debitor-no-reference
+  node no-debitor-no-reference
 echo "normal-iban-creditor-reference"
- node normal-iban-creditor-reference
+  node normal-iban-creditor-reference
 echo "normal-iban-no-reference"
- node normal-iban-no-reference
+  node normal-iban-no-reference
 echo "qr-iban"
- node qr-iban
+  node qr-iban
 echo "separate"
- node separate
+  node separate
