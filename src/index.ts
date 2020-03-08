0fb20a803bca92f67f272759cc53144e34235f26
Roger Schönbächler <schoero@gmx.ch>
2020-03-08 12:00:57 +0100

    Removed svgpath dependency

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { parse } from "svg-parser";
 import PDFDocument from "pdfkit";
 import QRCode from "qrcode-svg";
-import svgpath from "svgpath";
 import IBAN from "iban";
 import fs from "fs";
 
@@ -167,6 +166,8 @@ export class PDF {
     }
 
 
+    //-- Apply options
+
     if(options !== undefined){
       if(options.language !== undefined){
         this._language = options.language;
@@ -278,28 +279,26 @@ export class PDF {
 
     if(this._scissors === true){
 
-      let scissorsTop = "M4.545 -1.803C4.06 -2.388 3.185 -2.368 2.531 -2.116l-4.106 1.539c-1.194 -0.653 -2.374 -0.466 -2.374 -0.784c0 -0.249 0.228 -0.194 0.194 -0.842c-0.033 -0.622 -0.682 -1.082 -1.295 -1.041c-0.614 -0.004 -1.25 0.467 -1.255 1.115c-0.046 0.653 0.504 1.26 1.153 1.303c0.761 0.113 2.109 -0.348 2.741 0.785c-0.471 0.869 -1.307 0.872 -2.063 0.828c-0.627 -0.036 -1.381 0.144 -1.68 0.76c-0.289 0.591 -0.006 1.432 0.658 1.613c0.67 0.246 1.59 -0.065 1.75 -0.835c0.123 -0.594 -0.298 -0.873 -0.136 -1.089c0.122 -0.163 0.895 -0.068 2.274 -0.687L2.838 2.117C3.4 2.273 4.087 2.268 4.584 1.716L-0.026 -0.027L4.545 -1.803zm-9.154 -0.95c0.647 0.361 0.594 1.342 -0.078 1.532c-0.608 0.212 -1.386 -0.379 -1.192 -1.039c0.114 -0.541 0.827 -0.74 1.27 -0.493zm0.028 4.009c0.675 0.249 0.561 1.392 -0.126 1.546c-0.456 0.158 -1.107 -0.069 -1.153 -0.606c-0.089 -0.653 0.678 -1.242 1.279 -0.94z";
-      let scissorsCenter = "M1.803 4.545C2.388 4.06 2.368 3.185 2.116 2.531l-1.539 -4.106c0.653 -1.194 0.466 -2.374 0.784 -2.374c0.249 0 0.194 0.228 0.842 0.194c0.622 -0.033 1.082 -0.682 1.041 -1.295c0.004 -0.614 -0.467 -1.25 -1.115 -1.255c-0.653 -0.046 -1.26 0.504 -1.303 1.153c-0.113 0.761 0.348 2.109 -0.785 2.741c-0.869 -0.471 -0.872 -1.307 -0.828 -2.063c0.036 -0.627 -0.144 -1.381 -0.76 -1.68c-0.591 -0.289 -1.432 -0.006 -1.613 0.658c-0.246 0.67 0.065 1.59 0.835 1.75c0.594 0.123 0.873 -0.298 1.089 -0.136c0.163 0.122 0.068 0.895 0.687 2.274L-2.117 2.838C-2.273 3.4 -2.268 4.087 -1.716 4.584L0.027 -0.026L1.803 4.545zm0.95 -9.154c-0.361 0.647 -1.342 0.594 -1.532 -0.078c-0.212 -0.608 0.379 -1.386 1.039 -1.192c0.541 0.114 0.74 0.827 0.493 1.27zm-4.009 0.028c-0.249 0.675 -1.392 0.561 -1.546 -0.126c-0.158 -0.456 0.069 -1.107 0.606 -1.153c0.653 -0.089 1.242 0.678 0.94 1.279z";
+      const scissorsTop = "M4.545 -1.803C4.06 -2.388 3.185 -2.368 2.531 -2.116l-4.106 1.539c-1.194 -0.653 -2.374 -0.466 -2.374 -0.784c0 -0.249 0.228 -0.194 0.194 -0.842c-0.033 -0.622 -0.682 -1.082 -1.295 -1.041c-0.614 -0.004 -1.25 0.467 -1.255 1.115c-0.046 0.653 0.504 1.26 1.153 1.303c0.761 0.113 2.109 -0.348 2.741 0.785c-0.471 0.869 -1.307 0.872 -2.063 0.828c-0.627 -0.036 -1.381 0.144 -1.68 0.76c-0.289 0.591 -0.006 1.432 0.658 1.613c0.67 0.246 1.59 -0.065 1.75 -0.835c0.123 -0.594 -0.298 -0.873 -0.136 -1.089c0.122 -0.163 0.895 -0.068 2.274 -0.687L2.838 2.117C3.4 2.273 4.087 2.268 4.584 1.716L-0.026 -0.027L4.545 -1.803zm-9.154 -0.95c0.647 0.361 0.594 1.342 -0.078 1.532c-0.608 0.212 -1.386 -0.379 -1.192 -1.039c0.114 -0.541 0.827 -0.74 1.27 -0.493zm0.028 4.009c0.675 0.249 0.561 1.392 -0.126 1.546c-0.456 0.158 -1.107 -0.069 -1.153 -0.606c-0.089 -0.653 0.678 -1.242 1.279 -0.94z";
+      const scissorsCenter = "M1.803 4.545C2.388 4.06 2.368 3.185 2.116 2.531l-1.539 -4.106c0.653 -1.194 0.466 -2.374 0.784 -2.374c0.249 0 0.194 0.228 0.842 0.194c0.622 -0.033 1.082 -0.682 1.041 -1.295c0.004 -0.614 -0.467 -1.25 -1.115 -1.255c-0.653 -0.046 -1.26 0.504 -1.303 1.153c-0.113 0.761 0.348 2.109 -0.785 2.741c-0.869 -0.471 -0.872 -1.307 -0.828 -2.063c0.036 -0.627 -0.144 -1.381 -0.76 -1.68c-0.591 -0.289 -1.432 -0.006 -1.613 0.658c-0.246 0.67 0.065 1.59 0.835 1.75c0.594 0.123 0.873 -0.298 1.089 -0.136c0.163 0.122 0.068 0.895 0.687 2.274L-2.117 2.838C-2.273 3.4 -2.268 4.087 -1.716 4.584L0.027 -0.026L1.803 4.545zm0.95 -9.154c-0.361 0.647 -1.342 0.594 -1.532 -0.078c-0.212 -0.608 0.379 -1.386 1.039 -1.192c0.541 0.114 0.74 0.827 0.493 1.27zm-4.009 0.028c-0.249 0.675 -1.392 0.561 -1.546 -0.126c-0.158 -0.456 0.069 -1.107 0.606 -1.153c0.653 -0.089 1.242 0.678 0.94 1.279z";
 
       if(this._size === "A4"){
 
-        scissorsTop = svgpath(scissorsTop)
-          .translate(this.mmToPoints(105), this.mmToPoints(this._paddingTop))
-          .toString();
-
+        this.document.translate(this.mmToPoints(105), this.mmToPoints(this._paddingTop));
         this.document.path(scissorsTop)
           .fillColor("black")
           .fill();
+        this.document.translate(- this.mmToPoints(105), - this.mmToPoints(this._paddingTop));
 
       }
 
-      scissorsCenter = svgpath(scissorsCenter)
-        .translate(this.mmToPoints(62), this.mmToPoints(this._paddingTop)+ 30)
-        .toString();
 
+      this.document.translate(this.mmToPoints(62), this.mmToPoints(this._paddingTop) + 30);
       this.document.path(scissorsCenter)
         .fillColor("black")
         .fill();
+      this.document.translate(0, 0);
+      this.document.translate( - this.mmToPoints(62), - (this.mmToPoints(this._paddingTop) + 30));
 
     } else {
 
@@ -1063,7 +1062,7 @@ export class PDF {
       ecl: "M"
     }).svg();
 
-    let svgPath = this._getSVGPathFromQRCodeString(qrcodeString);
+    const svgPath = this._getSVGPathFromQRCodeString(qrcodeString);
 
     if(svgPath === undefined){
       throw new Error("Could not convert svg image to path");
@@ -1071,39 +1070,33 @@ export class PDF {
 
     this.document.moveTo(this.mmToPoints(67), this.mmToPoints(this._paddingTop + 17));
 
-    svgPath = svgpath(svgPath)
-      .translate(this.mmToPoints(67), this.mmToPoints(this._paddingTop + 17))
-      .toString();
-
 
     //-- Black rectangle
 
-    let background = "M18.3 0.7L1.6 0.7 0.7 0.7 0.7 1.6 0.7 18.3 0.7 19.1 1.6 19.1 18.3 19.1 19.1 19.1 19.1 18.3 19.1 1.6 19.1 0.7Z";
-    let cross = "M8.3 4H11.6V15H8.3V4Z M4.4 7.9H15.4V11.2H4.4V7.9Z";
-
-    background = svgpath(background)
-      .translate(this.mmToPoints(86), this.mmToPoints(this._paddingTop + 36))
-      .toString();
-
-    cross = svgpath(cross)
-      .translate(this.mmToPoints(86), this.mmToPoints(this._paddingTop + 36))
-      .toString();
+    const background = "M18.3 0.7L1.6 0.7 0.7 0.7 0.7 1.6 0.7 18.3 0.7 19.1 1.6 19.1 18.3 19.1 19.1 19.1 19.1 18.3 19.1 1.6 19.1 0.7Z";
+    const cross = "M8.3 4H11.6V15H8.3V4Z M4.4 7.9H15.4V11.2H4.4V7.9Z";
 
 
+    this.document.translate(this.mmToPoints(67), this.mmToPoints(this._paddingTop + 17));
     this.document.path(svgPath)
       .undash()
       .fillColor("black")
       .fill();
+    this.document.translate( - this.mmToPoints(67),  - this.mmToPoints(this._paddingTop + 17));
 
+    this.document.translate(this.mmToPoints(86), this.mmToPoints(this._paddingTop + 36));
     this.document.path(background)
       .fillColor("black")
       .lineWidth(1.4357)
       .strokeColor("white")
       .fillAndStroke();
+    this.document.translate( - this.mmToPoints(86),  -this.mmToPoints(this._paddingTop + 36));
 
+    this.document.translate(this.mmToPoints(86), this.mmToPoints(this._paddingTop + 36));
     this.document.path(cross)
       .fillColor("white")
       .fill();
+    this.document.translate( - this.mmToPoints(86), - this.mmToPoints(this._paddingTop + 36));
 
   }
 
