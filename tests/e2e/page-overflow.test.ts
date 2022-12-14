2c85c6d2e2d874f255d8e865664f672f14fee910
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2022-12-14 20:03:43 +0100

    test: add multi page test

---
+++ tests/e2e/page-overflow.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "vitest";
+
+import { mm2pt } from "swissqrbill:shared/utils.js";
+import { minimalRequired } from "swissqrbill:tests:data/data.js";
+import { createPDF } from "swissqrbill:tests:utils/pdf2.js";
+
+
+const OUT_DIR_PDF = "tests/output/pdf/multi-page";
+
+describe("Multiple pages", () => {
+
+  test("Page overflow", async () => {
+    const name = "page-overflow";
+    const { pdf, snapshots } = createPDF(minimalRequired, `${OUT_DIR_PDF}/${name}.pdf`, { autoGenerate: false, size: "A4" });
+
+    pdf.fontSize(11);
+    pdf.font("Helvetica-Bold");
+
+    pdf.text("PAGE 1", mm2pt(5), pdf.page.height - 50, {
+      align: "center",
+      width: mm2pt(210)
+    });
+
+    pdf.addQRBill("A4");
+
+    pdf.end();
+
+    await expect(snapshots).resolves.to.have.toHaveLength(2);
+    await expect(snapshots).resolves.toMatchSnapshot();
+
+  });
+
+});
--- tests/utils/pdf.ts
+++ tests/utils/pdf.ts
@@ -1,4 +1,5 @@
-import { writeFile } from "node:fs/promises";
+import { mkdir, writeFile } from "node:fs/promises";
+import { dirname, resolve as resolvePath } from "node:path";
 
 import { pdfToPng } from "pdf-to-png-converter";
 
@@ -10,26 +11,52 @@ import { WritableMemory } from "./writable-memory.js";
 
 const VISUAL = process.env.VISUAL === "true";
 
+export function createPDF(data: Data, path: string, options?: PDFOptions): { pdf: PDF; snapshots: Promise<string[]>; } {
+
+  const stream = new WritableMemory();
+  const pdf = new PDF(data, stream, { ...options });
+
+  const snapshots = new Promise<string[]>(resolve => {
+
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    pdf.on("finish", async () => {
+
+      const buffer = stream.read();
+      const snapshots = await pdfBufferToImages(buffer);
+
+      if(VISUAL === true){
+        await mkdir(dirname(resolvePath(path)), { recursive: true });
+        await writeFile(path, buffer);
+      }
+
+      resolve(snapshots);
+
+    });
+
+  });
+
+  return {
+    pdf,
+    snapshots
+  };
+
+}
+
+
 export async function pdf(data: Data, path: string, options?: PDFOptions) {
-  const pdfBuffer = await _createPDF(data, path, options);
+  const { snapshots } = createPDF(data, path, options);
+  return await snapshots;
+}
+
+
+export async function pdfBufferToImages(pdfBuffer: Buffer) {
+
   const images = await pdfToPng(pdfBuffer, {
     disableFontFace: false,
     useSystemFonts: true,
     viewportScale: 0.2
   });
+
   return images.map(page => page.content.toString("base64"));
-}
 
-
-async function _createPDF(data: Data, path: string, options?: PDFOptions): Promise<Buffer> {
-  const stream = new WritableMemory();
-  return new Promise((resolve, reject) => {
-    new PDF(data, stream, { ...options }, async () => {
-      const buffer = stream.read();
-      if(VISUAL === true){
-        await writeFile(path, buffer);
-      }
-      resolve(buffer);
-    });
-  });
 }
