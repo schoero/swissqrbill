37e241d60d3ea139ffc9c2541c7b9d7689dc8575
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2022-12-18 13:26:38 +0100

    refactor: use shared vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,6 @@
-import { resolve } from "node:path/posix";
-
-import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
-import GithubActionsReporter from "vitest-github-actions-reporter";
+import { config, defineConfig } from "@schoero/vite-config";
 
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths()
-  ],
-  test: {
-    reporters: process.env.GITHUB_ACTIONS
-      ? ["verbose", new GithubActionsReporter()]
-      : "default",
-    resolveSnapshotPath: (testPath, snapExtension) =>
-      resolve(testPath).includes(`/tests/`)
-        ? testPath.replace("/tests/", "/tests/__snapshots__/").replace(/\.ts$/, snapExtension)
-        : testPath.replace(/\.ts$/, snapExtension)
-  }
+  ...config
 });
