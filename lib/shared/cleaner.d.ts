559eb44a524ae107d16eee3ca3c4d984d60e73fe
Sili <silvan.klossner@klossner.ch>
2023-10-03 10:30:05 +0200

    Adjust for pull request

--- lib/shared/cleaner.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Data } from "./types";
-export declare function cleanData(data: Data): Data;
-export declare function removeSpaces(text: string): string;
-export declare function removeLineBreaks(text: string): string;
--- lib/shared/validator.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Data } from "./types";
-/**
- * Validate the provided data.
- * @param data The data to validate.
- * @throws {ValidationError} If the data is invalid.
- */
-export declare function validateData(data: Data): void;
