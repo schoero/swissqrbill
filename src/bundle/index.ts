deb428d767e3ab04bc9be3697d03e6bca6529cbe
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-10-29 19:37:18 +0100

    refactor: remove duplicate table export

--- src/bundle/index.ts
+++ src/bundle/index.ts
@@ -1,6 +1,5 @@
 /* eslint-disable simple-import-sort/exports */
 export * as pdf from "swissqrbill:pdf:index";
-export * as table from "swissqrbill:pdf:table";
 export * as svg from "swissqrbill:svg:index";
 export * as types from "swissqrbill:types";
 export * as utils from "swissqrbill:utils";
